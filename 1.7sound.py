eb88fff8d260f680deba3e5619b3d226ca1ab28f
Tomo <54297177+TI0360@users.noreply.github.com>
2023-02-04 22:42:12 +0900

    Add files via upload

--- 1.7sound.py
+++ 1.7sound.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 import configparser
 import urllib.request
-import PySimpleGUI as sg
+import customtkinter
 import os
 import getpass
 import sys
+import PySimpleGUI as sg
 
-file_version = "3"
+file_version = "4"
 
 user = getpass.getuser()
 
@@ -35,10 +36,22 @@ with open(f"C:\\Users\\{user}\\17-py-ti\\{confsave_name}", mode="wb") as f:
     f.write(data)
 
 config = configparser.ConfigParser()
-config.read(f'C:\\Users\\{user}\\17-py-ti\\config.ini', encoding='utf-8')
+try:
+    config.read(f'C:\\Users\\{user}\\17-py-ti\\config.ini', encoding='utf-8')
+except:
+    layout = [ [sg.Text("コンフィグファイルを読み込めませんでした。起動しなおしてください。")],
+        [sg.Button('OK', key='enter')]  ]
 
-sg.theme('BlueMono')
+    window = sg.Window('Startup Error!', layout)
 
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            sys.exit()
+
+        if event == 'enter':
+            sys.exit()
 
 fv = config.get('main', 'FILE')
 mv1 = config.get('main', 'VERSION1')
@@ -49,70 +62,81 @@ fn2 = config.get('download', 'FN2')
 fn3 = config.get('download', 'FN3')
 
 
-layout = [ [sg.Text("1.7sound-packダウンローダーへようこそ。安定最新版のファイルをダウンロードすることができます。")],
-        [sg.Text(f"最新バージョン：   [{fv}]")],
-        [sg.Text("バージョン選択："), sg.Combo((f'{mv1}', f'{mv2}', f'{mv3}'), default_value="選択してください...",size=(18, 1), key='select')],
-        [sg.Text("ファイル名："), sg.InputText('', key='name'), sg.Text("[拡張子は含めないでください]")],
-        [sg.Button('OK', key='enter'), sg.Button('終了', key='exit')]  ]
+customtkinter.set_appearance_mode("System")
+app = customtkinter.CTk()
+app.geometry("520x200")
+app.title("1.7sound-downloader")
 
-window = sg.Window('1.7sound-downloader', layout)
+def enter_f():
+    dversion = combobox.get()
+    if dversion == f'{mv1}':
+        url = f"https://github.com/TI0360/1.7sound-pack/releases/latest/download/{fn1}"
+        save_name = fn1
+        data = urllib.request.urlopen(url).read()
+        with open(f"{save_name}", mode="wb") as f:
+            f.write(data)
+        window = customtkinter.CTkToplevel()
+        window.title("Success")
+        window.geometry("400x100")
+        label = customtkinter.CTkLabel(window, text="ファイルのダウンロードが完了しました。")
+        label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
 
-while True:
-    event, values = window.read()
+    if dversion == f'{mv2}':
+        url = f"https://github.com/TI0360/1.7sound-pack/releases/latest/download/{fn2}"
+        save_name = fn2
+        data = urllib.request.urlopen(url).read()
+        with open(f"{save_name}", mode="wb") as f:
+            f.write(data)
+        window = customtkinter.CTkToplevel()
+        window.title("Success")
+        window.geometry("400x100")
+        label = customtkinter.CTkLabel(window, text="ファイルのダウンロードが完了しました。")
+        label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
 
-    if event == sg.WIN_CLOSED:
-        os.remove(f'C:\\Users\\{user}\\17-py-ti\\config.ini')
-        break
+    if dversion == f'{mv3}':
+        url = f"https://github.com/TI0360/1.7sound-pack/releases/download/1.9.2-1.20ready/{fn3}"
+        save_name = fn3
+        data = urllib.request.urlopen(url).read()
+        with open(f"{save_name}", mode="wb") as f:
+            f.write(data)
+        window = customtkinter.CTkToplevel()
+        window.title("Success")
+        window.geometry("400x100")
+        label = customtkinter.CTkLabel(window, text="ファイルのダウンロードが完了しました。")
+        label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
 
-    if event == 'exit':
-        os.remove(f'C:\\Users\\{user}\\17-py-ti\\config.ini')
-        break
+    else:
+        window = customtkinter.CTkToplevel()
+        window.title("Error")
+        window.geometry("400x100")
+        label = customtkinter.CTkLabel(window, text="バージョンを選択してください！")
+        label.pack(side="top", fill="both", expand=True, padx=40, pady=40)
+    
 
-    if event == 'enter':
-        if values['select'] == f'{mv1}':
-            url = f"https://github.com/TI0360/1.7sound-pack/releases/latest/download/{fn1}"
-            if values['name'] in "":
-                save_name = fn1
-                data = urllib.request.urlopen(url).read()
-                with open(f"C:\\Users\\{user}\\Desktop\\{save_name}", mode="wb") as f:
-                    f.write(data)
-                sg.popup("デスクトップへのダウンロードが完了しました。")
-            else:
-                data = urllib.request.urlopen(url).read()
-                with open(f"C:\\Users\\{user}\\Desktop\\{values['name']}.zip", mode="wb") as f:
-                    f.write(data)
-                sg.popup("デスクトップへのダウンロードが完了しました。")
 
-        if values['select'] == f'{mv2}':
-            url = f"https://github.com/TI0360/1.7sound-pack/releases/latest/download/{fn2}"
-            if values['name'] in "":
-                save_name = fn2
-                data = urllib.request.urlopen(url).read()
-                with open(f"C:\\Users\\{user}\\Desktop\\{save_name}", mode="wb") as f:
-                    f.write(data)
-                sg.popup("デスクトップへのダウンロードが完了しました。")
-            else:
-                data = urllib.request.urlopen(url).read()
-                with open(f"C:\\Users\\{user}\\Desktop\\{values['name']}.zip", mode="wb") as f:
-                    f.write(data)
-                sg.popup("デスクトップへのダウンロードが完了しました。")
+def exit_f():
+    os.remove(f'C:\\Users\\{user}\\17-py-ti\\config.ini')
+    sys.exit()
+    
 
-        if values['select'] == f'{mv3}':
-            url = f"https://github.com/TI0360/1.7sound-pack/releases/download/1.9.2-1.20ready/{fn3}"
-            if values['name'] in "":
-                save_name = fn3
-                data = urllib.request.urlopen(url).read()
-                with open(f"C:\\Users\\{user}\\Desktop\\{save_name}", mode="wb") as f:
-                    f.write(data)
-                sg.popup("デスクトップへのダウンロードが完了しました。")
-            else:
-                data = urllib.request.urlopen(url).read()
-                with open(f"C:\\Users\\{user}\\Desktop\\{values['name']}.zip", mode="wb") as f:
-                    f.write(data)
-                sg.popup("デスクトップへのダウンロードが完了しました。")
 
-window.close()
+label2 = customtkinter.CTkLabel(master=app, text=f"最新ファイルバージョン\n[ {fv} ]")
+label_ep = customtkinter.CTkLabel(master=app, text="")
+label3 = customtkinter.CTkLabel(master=app, text="MCバージョン選択")
+combobox = customtkinter.CTkOptionMenu(master=app,
+                                     values=[f'{mv1}', f'{mv2}', f'{mv3}'])
+combobox.set("選択してください...")
+button1 = customtkinter.CTkButton(master=app, text="実行", command=enter_f)
+button2 = customtkinter.CTkButton(master=app, text="終了", command=exit_f)
 
+label2.pack(side=customtkinter.LEFT, expand=True)
+label_ep.pack()
+label3.pack()
+combobox.pack()
+button1.pack(side=customtkinter.LEFT, expand=True)
+button2.pack(side=customtkinter.RIGHT, expand=True)
+
+app.mainloop()
 
 '''
 Copyright © 2001-2021 Python Software Foundation; All Rights Reserved
